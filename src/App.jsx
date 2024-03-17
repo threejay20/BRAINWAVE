5274b7b69797bc789df22fba1376c6aee395f4b9
threejay20 <118399217+threejay20@users.noreply.github.com>
2024-03-17 20:01:20 +0800

    footer and roadmap sections done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+// This code represents the main application component, "App", which serves as the entry point for the website.
+// It includes various sub-components such as Header, Hero, Benefits, Collaboration, Services, Pricing, Roadmap, and Footer,
+// arranged in the desired order to compose the entire website layout.
+// Additionally, it renders a ButtonGradient component at the end, likely serving as a floating action button or similar UI element.
+
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 const App = () => {
@@ -17,6 +24,8 @@ const App = () => {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
